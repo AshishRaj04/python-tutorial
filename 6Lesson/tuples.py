fc0dd64d2ed5d2aa8813bb44fc2d7de707a1063b
Ashish Raj <21c47@bcebakhtiyarpur.org>
2024-01-15 20:18:56 +0530

    tuples vs lists

---
+++ 6Lesson/tuples.py
@@ -0,0 +1,21 @@
+myTuple = tuple(('Raj' , 2003 , True))
+anotherTuple = (1,2,3,4,5,6 ,2,4,5,4)
+print(myTuple)
+print(anotherTuple)
+print(type(myTuple))
+print(type(anotherTuple))
+
+newList = list(myTuple)
+newList.append("Ashish")
+newTuple = tuple(newList)
+print(newTuple)
+
+(one , *two , hey) = anotherTuple #rest of the number goes to two
+print(one)
+print(two)
+print(hey)
+print(type(hey)) #int data type
+print(type(two)) #list data type
+
+print(anotherTuple.count(4)) #how many types 4 appear
+print(anotherTuple.index(1))
